2aca92ea450ccdc03cf1bbb269a44806e520974c
David Chandek-Stark <david.chandek.stark@duke.edu>
2024-04-12 10:26:10 -0400

    Initial commit of gem

---
+++ lib/openapi/grape/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Openapi
+  module Grape
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/openapi/grape_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Openapi::Grape do
+  it "has a version number" do
+    expect(Openapi::Grape::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
